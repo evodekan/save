66a9e2be80ff96fbefa3666800ca231a65ef1a57
evodekan <evodekanuma@gmail.com>
2021-08-02 11:57:02 +0200

    added config for github actions

--- lib/pages/saving_history.dart
+++ lib/pages/saving_history.dart
@@ -8,69 +8,63 @@ class SavingHistory extends StatefulWidget {
 }
 
 class _SavingHistoryState extends State<SavingHistory> {
-  get blue => null;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Column(
         children: <Widget>[
-          Container(
-            //height: 250,
+          Padding(
             padding: const EdgeInsets.all(16),
-            margin: const EdgeInsets.all(16),
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.grey,
-                width: 1,
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text(
+                          "Goals",
+                          style: TextStyle(fontSize: 20, color: Colors.blue),
+                        ),
+                        Text(
+                          "50000",
+                          style: TextStyle(fontSize: 20, color: Colors.blue),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text(
+                          "Saved",
+                          style: TextStyle(fontSize: 20, color: Colors.blue),
+                        ),
+                        Text(
+                          "240000",
+                          style: TextStyle(fontSize: 20, color: Colors.blue),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: const [
+                        Text(
+                          "260000",
+                          style: TextStyle(fontSize: 20, color: Colors.blue),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-              color: Colors.white,
-            ),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    Text(
-                      "Goals",
-                      style: TextStyle(fontSize: 20, color: Colors.blue),
-                    ),
-                    Text(
-                      "50000",
-                      style: TextStyle(fontSize: 20, color: Colors.blue),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 8,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    Text(
-                      "Saved",
-                      style: TextStyle(fontSize: 20, color: Colors.blue),
-                    ),
-                    Text(
-                      "240000",
-                      style: TextStyle(fontSize: 20, color: Colors.blue),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: const [
-                    Text(
-                      "260000",
-                      style: TextStyle(fontSize: 20, color: Colors.blue),
-                    ),
-                  ],
-                ),
-              ],
             ),
           )
         ],
